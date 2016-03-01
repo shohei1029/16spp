388e5db21bb49c923e52eb62b5f1cf7ca4a494db
Shohei1029 <t14650sn@sfc.keio.ac.jp>
2016-03-01 20:19:59 +0900

    update

---
+++ indicate_blastProgress.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import argparse
+
+from progressbar import ProgressBar
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-i","--blast_inputfile",type=str,help="Blast input file.")
+parser.add_argument("-o","--blast_outputfile",type=str,help="Blast output file.")
+args = parser.parse_args()
+
+
+with open(args.blast_inputfile, 'r') as bl_in_fh:
+    lines = bl_in_fh.read()
+    numentry = lines.count('>')
+
+quit()
+
+pbar = ProgressBar(max_value=numentry,redirect_stderr=True)
+pbar.start()
+while True:
+    with open(args.blast_outputfile, 'r') as bl_out_fh:
+        lines = bl_in_fh.read()
+        prog_numquery = lines.count('Query:')
+        pbar.update(prog_numquery)
+
+    if prog_numquery >= numentry:
+        break
+
+pbar.finish()
+        
+
