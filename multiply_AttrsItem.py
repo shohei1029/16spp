2aec1bd88592cebbf298b5bd0a890c46dd8be88a
Shohei1029 <t14650sn@sfc.keio.ac.jp>
2016-03-04 17:20:41 +0900

    update

---
+++ multiply_AttrsItem.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import sys
+
+#memo
+# Attrs の ID欄にドメインやセクションなどの名前をそれぞれ追加していく
+# 2016.3.4 by Shohei N.
+
+#hard coding
+variations = ["-int_sec", "-RT_sec", "-RNase_sec", "-prot_sec"] #including separators
+
+if __name__ == '__main__':
+    for line in sys.stdin:
+        header =  line
+        sys.stdout.write(header)
+        break
+    
+    for line in sys.stdin:
+        id =  line.split("\t")[0]
+        rest =  line.split("\t")[1:]
+    
+        for s in variations:
+            ls = []
+            id2 = id + s
+            ls.append(id2)
+            ls.extend(rest)
+            sys.stdout.write("\t".join(ls))
+
+
