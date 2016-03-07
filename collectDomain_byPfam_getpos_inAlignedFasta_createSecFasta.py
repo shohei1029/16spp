98f3cd66f3affa6767d540c72e68f102d1053ae9
Shohei1029 <t14650sn@sfc.keio.ac.jp>
2016-03-07 23:36:13 +0900

    update

--- collectDomain_byPfam_getpos_inAlignedFasta_createSecFasta.py
+++ collectDomain_byPfam_getpos_inAlignedFasta_createSecFasta.py
@@ -14,10 +14,12 @@ from Bio import SeqIO
 #アライメント後fataファイルを読み込ませ，ドメインの位置を数字で表示（最長に合わせる）
 #手動で,ドメインもとにpolを４分割したときの境目数字を計算し，ハードコーディングする
 #分割したときの各セクションについて，始め：終わりのリストの辞書を作成しておき，それを読み込ませて特定領域だけ切り出す。
-# →ドメインやる場合も同じ方式でできるように！！すばらしい
+# →ドメインやる場合も同じ方式でできるように！！すばらしい.←ドメインの配列長をドメインごとに揃えるとき限定な
+
+# 2016.3.7 ドメインの配列長をドメインごとに揃えないようにする。そういう関数も作った。
 
 stime = time.time()
-argvs = sys.argv
+#argvs = sys.argv
 
 #process number
 #proc = 8
@@ -72,11 +74,33 @@ def read_hmmscan_domtblout_todict(hmmscan_file): #void func
             domain_dict[query_acc][domain_name]["start"] = domain_start
             domain_dict[query_acc][domain_name]["end"]   = domain_end
 
+
 def read_fasta_todict(fasta_file):
     with open(fasta_file,'r') as fa_fh:
         fa_dict = SeqIO.to_dict(SeqIO.parse(fa_fh, "fasta"))
     return fa_dict
 
+#ドメイン領域の場所が入った辞書を返す。ドメイン領域は最長となるようにし，長さ揃える。
+def get_domain_gpdpos_widest(alnfasta_dict):
+    domain_gpdpos_ld = {}
+    for domn in domain_name_dict.keys():
+        dom_start,dom_end = 100000000000,0
+
+        #先に最長となるようなDomainの開始・終了点を決めて，次のfor文で配列とってきて書き込んでいく
+        for k_acc in alnfasta_dict.keys():
+            try:
+                dom_start_tmp = seqpos_to_gapped_seqpos(domain_dict[k_acc][domn]["start"],alnfasta_dict[k_acc].seq)
+                dom_end_tmp   = seqpos_to_gapped_seqpos(domain_dict[k_acc][domn]["end"],alnfasta_dict[k_acc].seq)
+                print(dom_end_tmp) #test
+
+                dom_start = dom_start_tmp if dom_start_tmp < dom_start else dom_start
+                dom_end   = dom_end_tmp   if dom_end_tmp   > dom_end   else dom_end
+            except KeyError as err: #主にそのドメインがヒットしなかった配列
+                pass
+        domain_gpdpos_ld[domn] = [dom_start, dom_end]
+    return domain_gpdpos_ld
+    
+#最長となるようなドメイン領域をとってきて，開始点・集結点を出力する。↑と被るからほぼdeprecated
 def print_domain_gpdpos(alnfasta_dict):
     fa2_dict = alnfasta_dict
 
@@ -107,11 +131,25 @@ def make_seqsec_fasta(seq_secs_ld, alnfasta_dict):
         break
 
 #strするときにpos-1で指定することを忘れずに！
-
     for seqsec_name, spos_l in seq_secs_ld.items():
         for k_acc in fa2_dict.keys():
             fasta_header = "{}-{}".format(k_acc, seqsec_name)
             sec_seq   = str(fa2_dict[k_acc].seq[spos_l[0]-1:spos_l[1]-1])
+            sec_seq_nogap = sec_seq.replace('-','')
+            outfa_fh.write(">{}\n{}\n".format(fasta_header,sec_seq_nogap))
+
+def make_domain_fasta(alnfasta_dict): #配列長，ドメインごとに揃えたりしない. アライメント後のfastaにする意味は特にない。
+#strするときにpos-1で指定することを忘れずに！
+    for domn in domain_name_dict.keys():
+        for k_acc in alnfasta_dict.keys():
+            try:
+                dom_start = seqpos_to_gapped_seqpos(domain_dict[k_acc][domn]["start"],alnfasta_dict[k_acc].seq)
+                dom_end   = seqpos_to_gapped_seqpos(domain_dict[k_acc][domn]["end"],alnfasta_dict[k_acc].seq)
+            except KeyError as err: #主にそのドメインがヒットしなかった配列
+                pass
+
+            fasta_header = "{}-{}".format(k_acc, domn)
+            sec_seq   = str(alnfasta_dict[k_acc].seq[dom_start-1:dom_end-1])
             sec_seq_nogap = sec_seq.replace('-','')
             outfa_fh.write(">{}\n{}\n".format(fasta_header,sec_seq_nogap))
 
@@ -124,8 +162,12 @@ if __name__ == '__main__':
     read_hmmscan_domtblout_todict(hmmscan_file)
     fa_d = read_fasta_todict(in_alnfasta)
 #    print_domain_gpdpos(fa_d)
+    make_domain_fasta(fa_d)
+    quit()
     
     seq_sec = {'prot_sec':[328,487], 'RT_sec':[488,873], 'RNase_sec':[874,1005], 'int_sec':[1006,1306]}
+    seq_dom = get_domain_gpdpos_widest(fa_d)
+    print(seq_dom)
     make_seqsec_fasta(seq_sec, fa_d)
     #Multiprocessing
     #pool = mp.Pool(proc)
---
+++ plot_hist.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import sys
+import time
+import os
+import argparse
+
+import matplotlib #to set use('Agg') 
+matplotlib.use('Agg') #place this before any other pylab/matplotlib/pyplot import
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+
+# 2016.3.7
+# Shohei N.
+#
+# sys.stdinになんかのリスト流し込むとヒストグラムにして出力してくれる。おわり。
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-b","--bin", type=int, help="bin of histgram (int)")
+parser.add_argument("-d","--kde", type=bool, default=False, help="Whether to plot a gaussian kernel density estimate.")
+args = parser.parse_args()
+
+def plot_distplot(x, outfile="./out_plot_hist.png", b=args.bin, kde=args.kde):
+    sns.set(style="whitegrid")
+    fig, ax = plt.subplots(1, 1, figsize=(8,6)) #fig->figure obj. ax->graph obj. 2,1とかだとgは配列に.2,2だとarray.
+    ax = sns.distplot(x, bins=b, kde=kde)
+    fig.savefig(outfile, dpi=180)
+    sns.plt.close()
+    
+
+if __name__ == '__main__':
+    l = []
+    for line in sys.stdin:
+        line = line.rstrip()
+        l.append(float(line))
+    plot_distplot(l)
+    
